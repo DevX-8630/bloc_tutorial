486283185009da56f49fc1586a6c58098837d00e
amantyagi-8630 <aman.tyagi8630@gmail.com>
2024-12-03 17:25:59 +0530

    bug fix

--- lib/bloc/login/login_bloc.dart
+++ lib/bloc/login/login_bloc.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
-import 'package:bloc_tutorial_app/utils/enums.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc_tutorial_app/utils/enums.dart';
 import 'package:http/http.dart' as http;
 
 part 'login_state.dart';
